cf77a56c2daa7cc1e5a52f04f1e2293a73de7dc6
Kysper <kysper50@gmail.com>
2018-08-25 00:22:50 -0500

    Finished Hangman polishing some animation code up

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # WordGuessingGame
 #Added needed HTML, CSS and javascript files to the repository.
+// Create an Array with a list of themed words for the game
+
+// Create an empty array to house the wrong guesses
+
+// Set your counter variables
+
+// Generate a random word from the list
+
+// Generate the appropriate hash marks for output _ _ _ _ _ _
+
+// Get users keyboard input
+
+// Loop through word to check for input match with word
+
+    // if guesses remaining = 0, user loses
+
+    // if else all letters guessed right, prompt victory screen
+
+    // if else letter already guessed, prompt user to try another letter
+
+    // if else match, display the letter in the appropriate position on screen
+
+        // Lower guesses remaining
+
+    // else not match, display the letter in the "already guessed" position.
+
+        // Lower guesses remaining
+
+// Ask if they want to play again
+
+// Bonus stuff below
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,115 @@
+var WordSelection = ["army", "nuclear", "delta", "alpha", "corpsman", "sniper",
+    "grunt", "private", "general", "commander", "tanks", "marines",
+    "navy", "airforce", "grenades", "missles", "nato", "zulu",
+    "battalion", "strike", "team", "breach",
+];
 
+var maxGuesses = 10;
 
-var maxTries = 10;
-
+var letterGuesses = [];
+var wordIndex;
+var wordGuess = [];
 var guessesLeft = 0;
-var gameStart = false;
-var userGuesses = [];
- var wordList = ["Gold","Codes","FoxOne", 'FoxTwo', 'Brevity', 'Delta',
-'Romeo','Alpha','Julia','Bravo','Sparrow','Tomahawk','Infrared',
-'Nuclear','Atomic','Catastrophic','Point','Impact',
-'Charlie','Zulu','Ranger','Marine','Sniper','Army',
-'Flag','Armory','Grenade','Breach','MOAB'];
-var letterChecker = [];
-var gameFinished = false;
+var gameHasStarted = false;
+var gameHasEnded = false;
+var wins = 0;
 
+function gameReset() {
+    guessesLeft = maxGuesses;
+    gameHasStarted = false;
+    letterGuesses = [];
+    wordGuess = [];
 
+    wordIndex = Math.floor(Math.random() * (WordSelection.length));
 
-
-function StartGame()
-{
-    gameStart = true;
-    console.log(gameStart);
-    maxTries = guessesLeft;
-}
-
-function GameEnd()
-{
-    gameFinished = true;
-    console.log(gameFinished);
-    ResetGame();
-}
-
-function ResetGame()
-{
-    guessesLeft = 0;
-    gameStart = false;
-    gameFinished = false;
-}
-function WordPicker()
-{
-    var word = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(word);
-}
-
-
-document.onkeyup = function(event) {
-    console.log(event);
-      event.key;
-    if(event.keyCode == 32){
-        
-        StartGame();
-        WordPicker();
+    for (var i = 0; i < WordSelection[wordIndex].length; i++) {
+        wordGuess.push("_ ");
     }
+
+    document.getElementById("winText").innerHTML = "";
+
+    updateScreen();
+};
+
+function updateScreen() {
+    document.getElementById("wins-link").innerHTML = wins;
+    document.getElementById("current-word").innerHTML = "";
+    for (var i = 0; i < wordGuess.length; i++) {
+        document.getElementById("current-word").innerHTML += wordGuess[i];
+    }
+    document.getElementById("guesses-remain").innerHTML = guessesLeft;
+    document.getElementById("guessed-letters").innerHTML = letterGuesses;
+
+    if (guessesLeft <= 0) {
+        document.getElementById("winText").innerHTML = "Sorry you lose!";
+        gameHasEnded = true;
+    }
+
+};
+
+function updateEnemy() {
+    document.getElementById("enemyImage").src = "../WordGuessingGame/assets/image/enemy.png";
 };
 
 
 
 
+document.onkeydown = function (event) {
+    if (gameHasEnded) {
+        gameReset();
+        gameHasEnded = false;
+    }
+    else {
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key.toLowerCase());
+        }
+    }
+};
 
+
+function makeGuess(letter) {
+    if (guessesLeft > 0) {
+        if (gameHasStarted = false) {
+            gameHasStarted = true;
+        }
+
+        if (letterGuesses.indexOf(letter) === -1) {
+            letterGuesses.push(letter);
+            evalGuess(letter);
+        }
+    }
+    updateScreen();
+
+    winChecker();
+
+};
+
+function winChecker() {
+    if (wordGuess.indexOf("_ ") == -1) {
+        wins++;
+        gameHasEnded = true;
+        document.getElementById("winText").innerHTML = "Congrats you win!";
+    }
+
+
+};
+
+function evalGuess(letter) {
+    var locations = [];
+
+    for (var i = 0; i < WordSelection[wordIndex].length; i++) {
+        if (WordSelection[wordIndex][i] === letter) {
+            locations.push(i);
+        }
+    }
+
+    if (locations.length <= 0) {
+        guessesLeft--;
+        updateEnemy();
+    }
+    else {
+        for (var i = 0; i < locations.length; i++) {
+            wordGuess[locations[i]] = letter;
+        }
+    };
+}
